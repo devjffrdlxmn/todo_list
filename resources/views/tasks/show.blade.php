8141ddd6f3feb1105ba679e365f6902725a1f66d
devjffrdlxmn <jeffrid.laxamana@gmail.com>
2025-04-18 16:13:11 +0800

    fix delete function

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -11,12 +11,13 @@
         </div>
         <div class="px-4 pb-4 pt-0 mt-2 flex justify-between">
            
-            <form action="POST" action="/tasks/{{ $task->id }}">
+            <form method="POST" action="/tasks/{{ $task->id }}">
                 @csrf
                 @method('Delete')
-                <button  type="submit" aria-activedescendant=""class="rounded-md bg-red-700 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
+                <button  type="submit" class="rounded-md bg-red-700 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
                     Delete    
                 </button>
+                <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Update</button>
             </form>
 
        
